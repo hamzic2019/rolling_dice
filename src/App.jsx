84f6e78d19b6b7342b5521eabc8f853c90ddb87e
Haris Hamzić <53975836+hamzic2019@users.noreply.github.com>
2020-09-05 20:29:51 +0200

    Dice app finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
 import Die from './Die';
+import './App.css'
 
-export default function App() {
-  return (
-    <div className="App">
-      <Die />
-    </div>
-  );
+export default class App extends Component {
+  static defaultProps = {
+    values: ['one', 'two', 'three', 'four', 'five', 'six']
+  }
+
+  state = {
+    diceOne: 'one',
+    diceTwo: 'one'
+  }
+
+  rollDice = () => {
+    const rndNum1 = Math.floor(Math.random() * 6);
+    const rndNum2 = Math.floor(Math.random() * 6);
+    this.setState({diceOne: this.props.values[rndNum1], diceTwo:this.props.values[rndNum2] })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="dices">
+          <Die value={this.state.diceOne} />
+          <Die value={this.state.diceTwo} />
+        </div>
+
+        <br />
+        <button className="rollDice" onClick={this.rollDice}>ROLL A DICE</button>
+      </div>
+    );
+  }
 }
+
+
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-
+import './Die.css'
 
 export default class Die extends Component {
   render(){
-    return <i class="fas fa-dice-three"></i>
+    return <i class={`die-item fas fa-dice-${this.props.value}`}></i>
   }
 }
